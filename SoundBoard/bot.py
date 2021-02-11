cc37013fdd0ab27481dc0466c864ec477aaf4477
Meme-Police <65254044+Meme-Police@users.noreply.github.com>
2021-02-11 10:58:11 -0700

    Update bot.py

--- SoundBoard/bot.py
+++ SoundBoard/bot.py
@@ -4,8 +4,8 @@ import os
 voiceQuotes = os.listdir("Sounds/")
 token = ""
 try:
-    # We can't include shaxxToken.txt in the repository as it is private
-     with open("engiToken.txt") as file:
+    # We can't include token.txt in the repository as it is private
+     with open("token.txt") as file:
         token = file.read()
 except FileNotFoundError as e:
     print(e)
